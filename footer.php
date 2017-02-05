58614d736d0fc88185261a43adef0ff97591f666
Anthony Pallone <pallone@unlv.nevada.edu>
2017-02-05 14:56:18 -0800

    Initial commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+<div id="footer">
+            <p><b>Tony Pallone's Personal Site | Hi World</b></p>
+    
+        </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tony Pallone's site</title>
+    <link href="http://tonypallone.com/style.css" type="text/css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Aldrich' rel='stylesheet' type='text/css'>
+    <link href="http://fonts.googleapis.com/css?family=Rokkitt" rel="stylesheet" type="text/css">
+    <link href='http://tonypallone.com/favicon.ico' rel='icon' type='image/x-icon'/>
+</head>
+
+<body>
+    <div id="header">
+        <div id="logo">
+                <a href="http://tonypallone.com"><img src="http://thedesigniac.com/tony/images/logosmall.png" alt="Tony Pallone's Site"></a>
+        </div>
+        <div id="navigation">
+            <ul style= "padding-left: 0px">
+                <li><a href="http://tonypallone.com/about">About</a></li>
+                <li><a href="http://tonypallone.com/skills">Skills</a></li>
+                <li><a href="http://tonypallone.com/portfolio">Portfolio</a></li>
+                <li><a href="http://tonypallone.com/contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+            <h1>Welcome to my corner of the internet.</h1>
